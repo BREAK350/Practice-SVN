94115d1b7aa6323e9c8fc56ec96e8e3b7b8daa85
BREAK350 <break350@gmail.com>
2015-02-17 13:22:24 +0200

    додав клас та інтерфейс для роботи з файлами: видалення списку файлів

---
+++ src/break350/svn/wwf/FileAction.java
@@ -0,0 +1,8 @@
+package break350.svn.wwf;
+
+import java.io.File;
+import java.util.List;
+
+public interface FileAction {
+	public void perform(List<File> files);
+}
---
+++ src/break350/svn/wwf/RemoveFileAction.java
@@ -0,0 +1,44 @@
+package break350.svn.wwf;
+
+import java.io.File;
+import java.util.List;
+
+public class RemoveFileAction implements FileAction {
+
+	@Override
+	public void perform(List<File> files) {
+		for (File file : files) {
+			remove(file, 0);
+		}
+	}
+
+	private String createPrefix(int level) {
+		String prefix = "";
+		if (level > 0) {
+			for (int i = 0; i < level; i++) {
+				prefix += "\t";
+			}
+		}
+		return prefix;
+	}
+
+	private void remove(File file, int level) {
+		String fname = "'" + file.getAbsolutePath() + "'";
+		String prefix = createPrefix(level);
+		if (file.exists()) {
+			File[] files = file.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				remove(files[i], level + 1);
+			}
+			if (file.delete()) {
+				System.out.println(prefix + "The file " + fname
+						+ " is successfully deleted");
+			} else {
+				System.out.println(prefix + "Error: the file " + fname
+						+ " is not deleted");
+			}
+		} else {
+			System.out.println(prefix + "File not found: " + fname);
+		}
+	}
+}
