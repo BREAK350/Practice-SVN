8c42db41c682f7497a0670f086bdb90dac56956f
BREAK350 <break350@gmail.com>
2015-02-17 16:48:54 +0200

    задав реальні шляхи для репозиторія та списку файлів

--- src/break350/repository/generator/FileListFactory.java
+++ src/break350/repository/generator/FileListFactory.java
@@ -3,7 +3,7 @@ package break350.repository.generator;
 import java.io.File;
 
 public class FileListFactory {
-	public final static String FILE_NAME = "list.txt";
+	public final static String FILE_NAME = "D:/Projects/Practic/Repository/test/files.txt";
 
 	public static FileListGenerator getFileListGenerator() {
 		File file = new File(FILE_NAME);
