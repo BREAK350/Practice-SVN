71856ad57514dae8d4d56eed983c75225d0ff519
BREAK350 <break350@gmail.com>
2015-02-19 15:54:50 +0200

    виправив помилку читання з null

--- src/break350/repository/files/FilesGeneratorFactory.java
+++ src/break350/repository/files/FilesGeneratorFactory.java
@@ -1,7 +1,20 @@
 package break350.repository.files;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilesGeneratorFactory {
 	public static FilesGenerator getGenerator() {
-		return null;
+		return new FilesGenerator() {
+
+			@Override
+			public List<String> generate() {
+				List<String> list = new ArrayList<String>();
+				list.add("");
+				list.add("");
+				list.add("");
+				return list;
+			}
+		};
 	}
 }
